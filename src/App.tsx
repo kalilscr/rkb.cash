e69e1a8ff2dd2ea8175df6a7f209a5ba45f88aae
rodrigo kalil <rkbuarque@gmail.com>
2021-07-21 18:25:29 -0300

    componetizando o modal, criando a estrutura do formulario e começando a estilização do modal.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
 
 Modal.setAppElement('#root');
@@ -24,12 +25,10 @@ export function App() {
 
      <Dashboard />
 
-     <Modal 
-       isOpen={isNewTransactionModalOpen}
-       onRequestClose={handleCloseNewTransactionModal}
-     >
-        <h2>Cadastrar Transação</h2>
-     </Modal>
+     <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+     />
 
      <GlobalStyle />
     </>
