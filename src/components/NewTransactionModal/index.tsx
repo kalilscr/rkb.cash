22c19377d769a2c4e2b3690f31c19828834de0d8
rodrigo kalil <rkbuarque@gmail.com>
2021-07-29 12:43:54 -0300

    adicionando funcionalidade e cores dos botões.

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,7 +2,8 @@ import Modal from 'react-modal';
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg';
-import { Container, TransacyionTypeContainer} from './styles';
+import { Container, TransacyionTypeContainer, RadioBox} from './styles';
+import { useState } from 'react';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -10,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionModalProps ) {
+    const [type, setType] = useState('deposit');
+
     return(
         <Modal 
         isOpen={isOpen}
@@ -34,15 +37,23 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
                 <input type="number" placeholder="Valor"/>
 
                 <TransacyionTypeContainer>
-                    <button type="button">
+                    <RadioBox type="button" 
+                    onClick={() => {setType('deposit'); }}
+                    isActive={type === 'deposit'}
+                    activeColor={'green'}
+                    >
                         <img src={incomeImg} alt="Entrada" />
                         <span>Entrada</span>
-                    </button>
+                    </RadioBox>
 
-                    <button type="button">
+                    <RadioBox type="button" 
+                    onClick={() => {setType('withdraw'); }}
+                    isActive={type === 'withdraw'}
+                    activeColor={'red'}
+                    >
                         <img src={outcomeImg} alt="Saída" />
                         <span>Saída</span>
-                    </button>
+                    </RadioBox>
                 </TransacyionTypeContainer>
 
                 <input placeholder="Categoria"/>
