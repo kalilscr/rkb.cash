5a97202e803e184e4efaa7da57118a819476f4e3
rodrigo kalil <rkbuarque@gmail.com>
2021-08-03 21:36:31 -0300

    calculando resumo do summary e criando hook.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { useState } from 'react';
 
 import Modal from 'react-modal';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { NewTransactionModal } from './components/NewTransactionModal';
-import { TransactionsProvider } from './TransactionsContext';
+import { TransactionsProvider } from './hooks/useTransactions';
 
 import { GlobalStyle } from './styles/global';
 
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,13 +1,38 @@
-import React, { useContext } from 'react';
+import { useTransactions } from '../../hooks/useTransactions';
+
 import IncomeImg from '../../assets/income.svg';
 import OutcomeImg from '../../assets/outcome.svg';
 import TotalImg from '../../assets/total.svg';
-import { TransactionsContext } from '../../TransactionsContext';
 
 import { Container } from './styles';
 
 export function Summary(){
-    const {transactions} = useContext(TransactionsContext);
+    const {transactions} = useTransactions();
+
+   /*  const totalDeposits = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'deposit') {
+            return acc + transaction.amount;
+        }
+
+        return acc;
+    }, 0); */
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'deposit') {
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+
+        return acc;
+
+    }, {
+        deposits: 0, 
+        withdraws: 0, 
+        total: 0,
+    })
 
     return(
         <Container>
@@ -16,7 +41,12 @@ export function Summary(){
                     <p>Entradas</p>
                     <img src={IncomeImg} alt="Entradas" />
                 </header>
-                <strong>R$1000,00</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                         style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.deposits)}
+                </strong>
             </div>
 
             <div>
@@ -24,7 +54,13 @@ export function Summary(){
                     <p>Saídas</p>
                     <img src={OutcomeImg} alt="Saídas" />
                 </header>
-                <strong>R$600,00</strong>
+                <strong>
+                    -
+                    {new Intl.NumberFormat('pt-BR', {
+                         style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.withdraws)}
+                </strong>
             </div>
 
             <div className="total-background">
@@ -32,7 +68,12 @@ export function Summary(){
                     <p>Total</p>
                     <img src={TotalImg} alt="Total" />
                 </header>
-                <strong>R$400,00</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.total)}
+                </strong>
             </div>
         </Container>
     )
